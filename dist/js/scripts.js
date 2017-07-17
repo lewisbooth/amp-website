fe9a9368160d2c0b5b3e6a323a8625b68b857184
Dan <Dan@Defaults-MacBook-Pro.local>
2017-07-17 16:44:50 +0100

    Initial setup

---
+++ dist/js/scripts.js
@@ -0,0 +1,2 @@
+//JS Main
+"use strict";
---
+++ gulpfile.js
@@ -0,0 +1,52 @@
+var gulp = require('gulp');
+    include = require('gulp-html-tag-include');
+    sass = require('gulp-sass');
+    sassFile = 'app/sass/*.scss';
+    cleanCSS = require('gulp-clean-css');
+    babel = require('gulp-babel');
+    browserSync = require('browser-sync').create();
+
+// sass
+gulp.task('sass', function() {
+  gulp.src(sassFile)
+    .pipe(sass())
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(gulp.dest('dist/css/'))
+    .pipe(browserSync.stream());
+});
+
+// include tags
+gulp.task('html-include', function() {
+    return gulp.src('./app/index.html')
+        .pipe(include())
+        .pipe(gulp.dest('./dist/'));
+});
+
+// Babel convert JS
+gulp.task('babel', function() {
+    gulp.src('app/js/*.js')
+        .pipe(babel({
+            presets: ['es2015']
+        }))
+        .pipe(gulp.dest('dist/js/'));
+});
+
+// browserSync
+gulp.task('serve', ['sass'], function() {
+     browserSync.init({
+        server: {
+            baseDir: "./dist"
+        }
+    });
+});
+
+// Watch
+gulp.task('watch', function () {
+    gulp.watch('./app/**/*.html', ['html-include']);
+    gulp.watch("dist/**/*.html").on('change', browserSync.reload);
+    gulp.watch('app/sass/*.scss', ['sass']);
+    gulp.watch('app/js/*.js', ['babel']);
+});
+
+// The default task (called when you run `gulp` from cli) 
+gulp.task('default', ['watch', 'serve']);
