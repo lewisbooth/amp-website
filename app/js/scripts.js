45a929c716af3247a3e235cbf1886e591577c4da
Dan <Dan@Defaults-MacBook-Pro.local>
2017-07-20 17:27:55 +0100

    Home page first draft

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -1,12 +1,14 @@
 //JS Main
+
+// GSAP
 const serviceBubbles = document.querySelectorAll(".bubble");
 
     $(serviceBubbles).hover(
       function() {
-        TweenMax.to($(this).find('.outer-full'), 1, {scale:1.2, transformOrigin: "center center", ease:Back.easeOut});
-        TweenMax.to($(this).find('.outer-top'), 1, {scale:1.1, transformOrigin: "center center", ease:Back.easeOut});
-        TweenMax.to($(this).find('.inner-left'), 1, {scale:0.9, transformOrigin: "center center", ease:Back.easeOut});
-        TweenMax.to($(this).find('.inner-right'), 1, {scale:0.9, transformOrigin: "center center", ease:Back.easeOut});
+        TweenMax.to($(this).find('.outer-full'), 1, {scale:1.2, transformOrigin: "center center", ease:Back.easeOut}).timeScale(1);
+        TweenMax.to($(this).find('.outer-top'), 1, {scale:1.1, transformOrigin: "center center", ease:Back.easeOut}).timeScale(1);
+        TweenMax.to($(this).find('.inner-left'), 1, {scale:0.9, transformOrigin: "center center", ease:Back.easeOut}).timeScale(1);
+        TweenMax.to($(this).find('.inner-right'), 1, {scale:0.9, transformOrigin: "center center", ease:Back.easeOut}).timeScale(1);
         TweenMax.to($(this).find('.glow'), 1, {opacity: 1, ease:Back.easeOut});
         TweenMax.to($(this).find('.outer-full'), 10, {rotation:'360', transformOrigin: "center 49%", ease:Linear.easeNone,repeat:-1}).timeScale(1);
         TweenMax.to($(this).find('.outer-top'), 3.5, {rotation:'-360', transformOrigin: "center center", ease: Power2.easeInOut, repeat:-1}).timeScale(1);
@@ -14,10 +16,10 @@ const serviceBubbles = document.querySelectorAll(".bubble");
         TweenMax.to($(this).find('.inner-right'), 5, {rotation:'360', transformOrigin: "center center", ease:Linear.easeNone,repeat:-1}).timeScale(1);
       },
       function() {
-        TweenMax.to($(this).find('.outer-full'), .5, {scale:1, transformOrigin: "center center", ease:Linear.easeOut}); 
-        TweenMax.to($(this).find('.outer-top'), .5, {scale:1, transformOrigin: "center center", ease:Linear.easeOut});  
-        TweenMax.to($(this).find('.inner-left'), .5, {scale:1, transformOrigin: "center center", ease:Linear.easeOut});
-        TweenMax.to($(this).find('.inner-right'), .5, {scale:1, transformOrigin: "center center", ease:Linear.easeOut});
+        TweenMax.to($(this).find('.outer-full'), .5, {scale:1, transformOrigin: "center center", ease:Linear.easeOut}).timeScale(1); 
+        TweenMax.to($(this).find('.outer-top'), .5, {scale:1, transformOrigin: "center center", ease:Linear.easeOut}).timeScale(1);
+        TweenMax.to($(this).find('.inner-left'), .5, {scale:1, transformOrigin: "center center", ease:Linear.easeOut}).timeScale(1);
+        TweenMax.to($(this).find('.inner-right'), .5, {scale:1, transformOrigin: "center center", ease:Linear.easeOut}).timeScale(1);
         TweenMax.to($(this).find('.glow'), .3, {opacity: 0.5, ease:Back.easeOut});
         TweenMax.to($(this).find('.outer-full'), .3, {rotation:'0', transformOrigin: "center 49%", ease:Linear.easeIn,repeat:0}).timeScale(1);
         TweenMax.to($(this).find('.outer-top'), .3, {rotation:'0', transformOrigin: "center center", ease:Linear.easeIn,repeat:0}).timeScale(1);
@@ -25,3 +27,18 @@ const serviceBubbles = document.querySelectorAll(".bubble");
         TweenMax.to($(this).find('.inner-right'), .3, {rotation:'0', transformOrigin: "center center", ease:Linear.easeIn,repeat:0}).timeScale(1);
       }
     );
+
+    //Textarea expand
+    var textarea = document.querySelector('textarea');
+
+    textarea.addEventListener('keydown', autosize);
+                
+    function autosize(){
+      var el = this;
+      setTimeout(function(){
+        el.style.cssText = 'height:auto; padding:0';
+        // for box-sizing other than "content-box" use:
+        // el.style.cssText = '-moz-box-sizing:content-box';
+        el.style.cssText = 'height:' + el.scrollHeight + 'px';
+      },0);
+    }
